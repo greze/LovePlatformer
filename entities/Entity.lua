783dd8e86c527c36180fd18e1294230af668ddc7
greze <lmgiles6@gmail.com>
2017-02-26 18:20:20 +0900

    add libraries and Enity.lua basic stuff

---
+++ entities/Entity.lua
@@ -0,0 +1,28 @@
+
+--Represents a single drawable object
+local Class = require 'libs.middleclass.middleclass'
+
+local Entity = Class('Entity')
+
+--Entities must have a :initialize, :draw, :update
+function Entity:initialize(world, x, y, w, h)
+  self.world = world
+  self.x = x
+  self.y = y
+  self.w = w
+  self.h = h
+end
+
+function Entity:getRect()
+  return self.x, self.y, self.w, self.h
+end
+
+function Entity:draw()
+  -- Do Nothing by default
+end
+
+function Entity:update(dt)
+  --Do nothing by default
+end
+
+return Entity
